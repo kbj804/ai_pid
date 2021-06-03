d280f00ee8a4e82be315a45ec643c6ec79f4a2aa
kbj <rain1344@naver.com>
2021-06-03 16:39:36 +0900

    lunch selector ui 및 기능 개선

--- Scripts/front-vue/src/components/doc/BlogComponent.vue
+++ Scripts/front-vue/src/components/doc/BlogComponent.vue
@@ -2,7 +2,7 @@
   <div class="center">
     <md-table v-model="restaurants" md-card md-fixed-header @md-selected="onSelect">
       <md-table-toolbar>
-          <h1 class="md-title">점심 뭐먹지?</h1>
+          <h1 class="md-title">Lunch Selector</h1>
       </md-table-toolbar>
 
       <md-table-toolbar slot="md-table-alternate-header" slot-scope="{ count }">
@@ -15,23 +15,36 @@
         </div>
       </md-table-toolbar>
 
+
       <md-table-row slot="md-table-row" slot-scope="{ item }" class="md-accent" md-selectable="multiple" md-auto-select>
         <md-table-cell style="width:50px;"  md-label="Name" md-sort-by="name">{{item.name}}</md-table-cell>
         <md-table-cell  md-label="Progress Bar" md-sort-by="pb"><md-progress-bar class="md-accent" md-mode="buffer" :md-value="item.amount" :md-buffer="buffer"/></md-table-cell>
       </md-table-row>
     </md-table>
     <md-button class="md-raised" :md-ripple="false" @click="go">GO!</md-button>
-    <md-button class="md-primary md-raised" @click="active = true">Add</md-button>
+    <md-button class="md-accent" style="float: right" > WINNER: {{winner}} </md-button>
+    
+    <md-dialog-alert
+      :md-active.sync="flag"
+      md-title="★ WINNER ★"
+      :md-content="winner" />
+
+    <!-- <md-button class="md-primary md-raised" @click="active = true">Add</md-button>
 
     <md-dialog-prompt
       :md-active.sync="active"
       v-model="value"
-      md-title="식당 추가"
+      md-title="Restaurant ADD"
       md-input-maxlength="15"
-      md-input-placeholder="식당 이름"
-      md-confirm-text="추가"
+      md-input-placeholder="Restaurant Name"
+      md-confirm-text="ADD"
       @md-confirm="addRestaurant"
-      />
+    /> -->
+
+    <md-chips class="md-accent" v-model="rname" md-placeholder="Add Here..." :md-auto-insert="true" @md-insert="addRestaurant2">
+      <label>Add Name</label>
+    </md-chips>
+    <md-chip class="md-accent" v-for="chip in rstrChips" :key="chip" md-clickable @click="addChip(chip)">{{ chip }}</md-chip>
 
   </div>
 </template>
@@ -43,20 +56,17 @@
     name: 'ChoiceDisorder',
     data() {
       return{
+        flag: false,
+        rname:[],
         selected: [],
         buffer: 0,
         active: false,
         value: null,
         restaurants: [
-          {
-            name: "오돈",
-            amount: 0
-          },
-          {
-            name: "대문집",
-            amount: 0
-          }
+          {name: "sample", amount:0}
         ],
+        rstrChips:['배꼽집', '대문집', '진주집', '중국집', '쌀국수', '고등어', '분식', '순대국'],
+        winner: null,
       }
     },
     created() {
@@ -77,15 +87,19 @@
           plural = 's'
         }
 
-        return `${count} resturants${plural} selected`
+        return `${count} contents${plural} selected`
       },
       onSelect (item) {
         this.selected = item
       },
 
-      addRestaurant() {
-        this.restaurants.push({name: this.value, amount:0})
-        this.value = null;
+      // addRestaurant() {
+      //   this.restaurants.push({name: this.value, amount:0})
+      //   this.value = null;
+      // },
+      addRestaurant2(str){
+        this.restaurants.push({name: str, amount:0})
+        this.rname=[]
       },
 
       randomPick(num) {
@@ -99,28 +113,30 @@
         while(arr.every(element => element <= 100)) {
             const num = this.randomPick(n);
             ++arr[num - 1];
+            let winnerIndex = arr.indexOf(Math.max(...arr));
 
-            const timeoutId = setTimeout(num => {
-              ++this.restaurants[num - 1].amount;
+            const timeoutId = setTimeout(obj => {
+              ++this.restaurants[obj.num - 1].amount;
+              this.winner = this.restaurants[obj.winnerIndex].name
 
-              if(!(100 >= this.restaurants[num - 1].amount)) {
+              if(!(100 >= this.restaurants[obj.num - 1].amount)) {
+                this.flag = true;
                 clearTimeout(timeoutId);
+                
                 return;
               }
-            }, idx++ * 100, num)
+            }, idx++ * 100, {num, winnerIndex})
         }
+        
       },
       deleter() {
         this.selected.forEach(ele => {
             const idx = this.restaurants.findIndex(element => element.name === ele.name)
             if (idx > -1) this.restaurants.splice(idx, 1)
         })
-        // console.log(this.selected.forEach(elements => this.restaurants.filter(element => element.name !== elements.name)))
-        // this.restaurants.splice(this.restaurants.indexOf("대문집"))
-        // console.log(this.restaurants.filter(element => element.name !== '오돈'))
-        // this.restaurants = this.selected.forEach(elements => this.restaurants.forEach(element => element !== elements.name))
-        // this.restaurants = this.selected.filter(elements => this.restaurants.filter(element => element !== elements.name))
-
+      },
+      addChip(str){
+        this.restaurants.push({name: str, amount:0})
       },
       
     }
